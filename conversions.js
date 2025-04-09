1a4bfe28d7793671441ed6e56ff631a34007520a
FrancescCantallops <francesccantallops@alumnat.iesdamiahuguet.net>
2025-04-09 19:10:43 +0200

    Conversio massa i distancia

--- conversions.js
+++ conversions.js
@@ -15,3 +15,33 @@ function rpm_to_degreesXsecond(rpm){
 console.log("Revolutions per minute: " + rpm);
 console.log("RPS: " + rpm_to_rps(rpm));
 console.log("Degrees X second: " + rpm_to_degreesXsecond(rpm));
+
+function to_Kg(value, unit){
+    if(unit == "Kg"){
+        return value;
+    }
+    else if(unit == "ton"){
+        return value * 1000;
+    }
+    else if(unit == "g"){
+        return value / 1000;
+    }
+    else{
+        console.log("Invalid mass unit");
+    }
+}
+
+function to_meters(value, unit){
+    if(unit == "m"){
+        return value;
+    }
+    else if(unit == "cm"){
+        return value / 100;
+    }
+    else if(unit == "mm"){
+        return value / 1000;
+    }
+    else{
+        console.log("Invalid length unit");
+    }
+}
